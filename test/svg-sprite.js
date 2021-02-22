033dd144589ec84f1ae0c529fd090ba8e3697f41
XhmikosR <xhmikosr@gmail.com>
2021-02-22 10:28:01 +0200

    test: add a separate css.packed.12.png file for Node.js > 10
    Since Node.js 12, the Array.sort algorithm has changed and it results to a different image.
    
    # Conflicts:
    #	test/svg-sprite.js

--- test/svg-sprite.js
+++ test/svg-sprite.js
@@ -36,6 +36,9 @@ var cwdWeather = path.join(__dirname, 'fixture', 'svg', 'single'),
     cwdAlign = path.join(__dirname, 'fixture', 'svg', 'css'),
     dest = path.normalize(path.join(__dirname, '..', 'tmp'));
 
+// This is so that we can fix tests on Node.js > 10 since the Array.sort algorithm changed
+var isNodeGreaterThan10 = process.version.split('.')[0].slice(1) > 10;
+
 /**
  * Add a bunch of SVG files
  *
@@ -338,7 +341,7 @@ describe('svg-sprite', function () {
                     compareSvg2Png(
                         path.join(__dirname, '..', 'tmp', 'css', 'svg', svg.packed),
                         path.join(__dirname, '..', 'tmp', 'css', 'png', 'css.packed.png'),
-                        path.join(__dirname, 'expected', 'png', 'css.packed.png'),
+                        path.join(__dirname, 'expected', 'png', isNodeGreaterThan10 ? 'css.packed.12.png' : 'css.packed.png'),
                         path.join(__dirname, '..', 'tmp', 'css', 'png', 'css.packed.diff.png'),
                         done,
                         'The packed sprite doesn\'t match the expected one!'
@@ -508,7 +511,7 @@ describe('svg-sprite', function () {
                     compareSvg2Png(
                         path.join(__dirname, '..', 'tmp', 'view', 'svg', svg.packed),
                         path.join(__dirname, '..', 'tmp', 'view', 'png', 'view.packed.png'),
-                        path.join(__dirname, 'expected', 'png', 'css.packed.png'),
+                        path.join(__dirname, 'expected', 'png', isNodeGreaterThan10 ? 'css.packed.12.png' : 'css.packed.png'),
                         path.join(__dirname, '..', 'tmp', 'view', 'png', 'view.packed.diff.png'),
                         done,
                         'The packed sprite doesn\'t match the expected one!'
