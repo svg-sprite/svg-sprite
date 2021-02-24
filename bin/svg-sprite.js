62e65c4a5a42f3ff54a913caa65c30229556252e
XhmikosR <xhmikosr@gmail.com>
2021-02-24 15:19:27 +0200

    Comply to the new linting rules
    Also,
    
    * use arrow functions and destructuring
    * reduce some variables' scope
    * move a few variables out of for loop
    * move code to variables to reduce line lengths
    * move requires at the top
    * stop using uppercase names for variables
    * remove a few extra parentheses
    * for loops: switch to postfix increment operators and remove the temp length variable.
    * remove commented out function parameters
    * remove the individual test timeout since we set it globally in the npm script

--- bin/svg-sprite.js
+++ bin/svg-sprite.js
@@ -15,26 +15,27 @@
 /**
  * Module dependencies.
  */
-var _ = require('lodash'),
-	path = require('path'),
-	fs = require('fs'),
-	File = require('vinyl'),
-	yaml = require('js-yaml'),
-	glob = require('glob'),
-	SVGSpriter = require('../lib/svg-sprite'),
-	config = {},
-	JSONConfig = { mode: {} },
-	map = {},
-	yargs = require('yargs')
-		.usage('Create one or multiple sprites of the given SVG files, optionally along with some stylesheet resources.\nUsage: $0 [options] files')
-		.version()
-		.help('help', 'Display this help information')
-		.wrap(null)
-		.example('$0 --css --css-render-css --css-example --dest=out assets/*.svg', 'Create a CSS sprite of the given SVG files including example document to the subdirectory "out"')
-		.example('$0 -cD out --ccss --cx assets/*.svg', 'Same as above')
-		.example('$0 -cD out --cscss -p 10 assets/*.svg', 'Render Sass instead of CSS and add 10px padding around all shapes (no example document this time)')
-		.showHelpOnFail(true)
-		.demandCommand(1);
+const fs = require('fs');
+const path = require('path');
+const _ = require('lodash');
+const File = require('vinyl');
+const yaml = require('js-yaml');
+const glob = require('glob');
+const SVGSpriter = require('../lib/svg-sprite');
+
+const config = {};
+let JSONConfig = { mode: {} };
+const map = {};
+let yargs = require('yargs')
+    .usage('Create one or multiple sprites of the given SVG files, optionally along with some stylesheet resources.\nUsage: $0 [options] files')
+    .version()
+    .help('help', 'Display this help information')
+    .wrap(null)
+    .example('$0 --css --css-render-css --css-example --dest=out assets/*.svg', 'Create a CSS sprite of the given SVG files including example document to the subdirectory "out"')
+    .example('$0 -cD out --ccss --cx assets/*.svg', 'Same as above')
+    .example('$0 -cD out --cscss -p 10 assets/*.svg', 'Render Sass instead of CSS and add 10px padding around all shapes (no example document this time)')
+    .showHelpOnFail(true)
+    .demandCommand(1);
 
 /**
  * Add a command line option
@@ -44,39 +45,38 @@ var _ = require('lodash'),
  * @return {void}
  */
 function addOption(name, option) {
-	var alias = name;
+    let alias = name;
 
-	// If the this is an option itself
-	if ('description' in option) {
-		if ('alias' in option) {
-			alias = option.alias;
-			yargs = yargs.alias(alias, name);
-		}
+    // If the this is an option itself
+    if ('description' in option) {
+        if ('alias' in option) {
+            alias = option.alias;
+            yargs = yargs.alias(alias, name);
+        }
 
-		yargs = yargs.describe(alias, option.description);
+        yargs = yargs.describe(alias, option.description);
 
-		if ('default' in option) {
-			var template = name.endsWith('-template'),
-				def = template ? path.resolve(path.dirname(__dirname), option.default) : option.default;
-			yargs = yargs.default(alias, def);
+        if ('default' in option) {
+            const template = name.endsWith('-template');
+            const def = template ? path.resolve(path.dirname(__dirname), option.default) : option.default;
+            yargs = yargs.default(alias, def);
 
-			if ((option.default === true) || (option.default === false)) {
-				yargs = yargs.boolean(name);
-			}
+            if (option.default === true || option.default === false) {
+                yargs = yargs.boolean(name);
+            }
+        } else if (option.required) {
+            yargs = yargs.require(alias);
+        }
 
-		} else if (option.required) {
-			yargs = yargs.require(alias);
-		}
+        if ('map' in option) {
+            map[option.map] = name;
+        }
+    }
 
-		if ('map' in option) {
-			map[option.map] = name;
-		}
-	}
-
-	var children = _.omit(option, ['description', 'alias', 'default', 'map']);
-	for (var sub in children) {
-		addOption(name + '-' + sub, children[sub]);
-	}
+    const children = _.omit(option, ['description', 'alias', 'default', 'map']);
+    for (const sub in children) {
+        addOption(`${name}-${sub}`, children[sub]);
+    }
 }
 
 /**
@@ -87,15 +87,16 @@ function addOption(name, option) {
  * @param {Mixed} value Value
  */
 function addConfigMap(store, path, value) {
-	var key = path.shift();
-	if (path.length) {
-		if (!(key in store) || !_.isObject(store[key])) {
-			store[key] = {};
-		}
-		addConfigMap(store[key], path, value);
-	} else {
-		store[key] = value;
-	}
+    const key = path.shift();
+    if (path.length) {
+        if (!(key in store) || !_.isObject(store[key])) {
+            store[key] = {};
+        }
+
+        addConfigMap(store[key], path, value);
+    } else {
+        store[key] = value;
+    }
 }
 
 /**
@@ -105,179 +106,182 @@ function addConfigMap(store, path, value) {
  * @return {Number} Number of written files
  */
 function writeFiles(files) {
-	var written = 0;
-	for (var key in files) {
-		if (_.isObject(files[key])) {
-			if (files[key].constructor === File) {
-				fs.mkdirSync(path.dirname(files[key].path), { recursive: true });
-				fs.writeFileSync(files[key].path, files[key].contents);
-				++written;
-			} else {
-				written += writeFiles(files[key]);
-			}
-		}
-	}
-	return written;
+    let written = 0;
+    for (const key in files) {
+        const file = files[key];
+
+        if (_.isObject(file)) {
+            if (file.constructor === File) {
+                fs.mkdirSync(path.dirname(file.path), { recursive: true });
+                fs.writeFileSync(file.path, file.contents);
+                ++written;
+            } else {
+                written += writeFiles(file);
+            }
+        }
+    }
+
+    return written;
 }
 
 // Get document, or throw exception on error
 try {
-	var options = yaml.load(fs.readFileSync(path.resolve(__dirname, 'config.yaml'), 'utf8'));
-	for (var name in options) {
-		addOption(name, options[name]);
-	}
-
-} catch (e) {
-	console.log(e);
+    const options = yaml.load(fs.readFileSync(path.resolve(__dirname, 'config.yaml'), 'utf8'));
+    for (const name in options) {
+        addOption(name, options[name]);
+    }
+} catch (error) {
+    console.log(error);
 }
 
-var argv = yargs.argv;
+const { argv } = yargs;
 
 // Map all arguments to a global configuration object
-for (var m in map) {
-	if (!(map[m] in argv)) {
-		continue;
-	}
-	addConfigMap(config, m.split('.'), argv[map[m]]);
+for (const m in map) {
+    if (!(map[m] in argv)) {
+        continue;
+    }
+
+    addConfigMap(config, m.split('.'), argv[map[m]]);
 }
 
 // Load external JSON config file
-if (argv['config']) {
-	try {
-		var file = argv['config'];
-		delete argv['config'];
-		delete argv['C'];
-		var JSONConfigContent = fs.readFileSync(path.resolve(file));
-		var externalConfig = JSON.parse(JSONConfigContent);
+if (argv.config) {
+    try {
+        const file = argv.config;
+        delete argv.config;
+        delete argv.C;
+        const JSONConfigContent = fs.readFileSync(path.resolve(file));
+        const externalConfig = JSON.parse(JSONConfigContent);
 
-		// Make a clone of initial config for options removal checks
-		JSONConfig = JSON.parse(JSONConfigContent);
-		if (!('mode' in JSONConfig)) {
-			JSONConfig['mode'] = {};
-		}
+        // Make a clone of initial config for options removal checks
+        JSONConfig = JSON.parse(JSONConfigContent);
+        if (!('mode' in JSONConfig)) {
+            JSONConfig.mode = {};
+        }
 
-		// Expand shorthand mode definitions
-		if (('mode' in externalConfig) && _.isObject(externalConfig.mode)) {
-			for (var emode in externalConfig.mode) {
-				if (externalConfig.mode[emode] === true) {
-					externalConfig.mode[emode] = JSONConfig.mode[emode] = {
-						render: {
-							css: true
-						}
-					};
-				}
-			}
-		}
+        // Expand shorthand mode definitions
+        if ('mode' in externalConfig && _.isObject(externalConfig.mode)) {
+            for (const emode in externalConfig.mode) {
+                if (externalConfig.mode[emode] === true) {
+                    const defaultEmode = {
+                        render: {
+                            css: true
+                        }
+                    };
+                    externalConfig.mode[emode] = defaultEmode;
+                    JSONConfig.mode[emode] = defaultEmode;
+                }
+            }
+        }
 
-		_.merge(config, externalConfig);
-	} catch (e) {
-		console.error('[ERROR] Skipping --config file due to errors ("%s")', e.message.trim());
-	}
+        _.merge(config, externalConfig);
+    } catch (error) {
+        console.error('[ERROR] Skipping --config file due to errors ("%s")', error.message.trim());
+    }
 }
 
 // Refine particular config options
-config.shape.spacing.padding = ('' + config.shape.spacing.padding).trim();
-config.shape.spacing.padding = config.shape.spacing.padding.length ? config.shape.spacing.padding.split(',').map(function (dim) {
-	return parseFloat(dim || 0);
-}) : [];
+config.shape.spacing.padding = String(config.shape.spacing.padding).trim();
+config.shape.spacing.padding = config.shape.spacing.padding.length ?
+    config.shape.spacing.padding.split(',').map(dim => Number.parseFloat(dim || 0)) :
+    [];
 
 if (config.svg.rootAttributes && typeof config.svg.rootAttributes === 'string') {
-	try {
-		var JSONAttributesContent = fs.readFileSync(path.resolve(config.svg.rootAttributes));
-		config.svg.rootAttributes = JSON.parse(JSONAttributesContent);
-	} catch (e) {
-		console.error('[ERROR] Skipping --svg-rootattrs file due to errors ("%s")', e.message.trim());
-		config.svg.rootAttributes = {};
-	}
+    try {
+        const JSONAttributesContent = fs.readFileSync(path.resolve(config.svg.rootAttributes));
+        config.svg.rootAttributes = JSON.parse(JSONAttributesContent);
+    } catch (error) {
+        console.error('[ERROR] Skipping --svg-rootattrs file due to errors ("%s")', error.message.trim());
+        config.svg.rootAttributes = {};
+    }
 }
 
 // Expand transformation options
 if (typeof config.shape.transform === 'string') {
-	var transform = ('' + config.shape.transform).trim();
-	config.shape.transform = [];
-	(transform.length ? transform.split(',').map(function (trans) {
-		return ('' + trans).trim();
-	}) : []).forEach(function (transform) {
-		if (transform.length) {
-			if (('shape-transform-' + transform) in argv) {
-				try {
-					var transformConfigFile = argv['shape-transform-' + transform],
-						transformConfigJSON = fs.readFileSync(path.resolve(transformConfigFile), { encoding: 'utf8' }),
-						transformConfig = transformConfigJSON.trim() ? JSON.parse(transformConfigJSON) : {};
-					this.push(_.zipObject([transform], [transformConfig]));
-				} catch (e) {
-				}
-			} else {
-				this.push(transform);
-			}
-		}
-	}, config.shape.transform);
+    const transform = String(config.shape.transform).trim();
+    config.shape.transform = [];
+    (transform.length ? transform.split(',').map(trans => String(trans).trim()) : [])
+        .forEach(function(transform) {
+            if (transform.length) {
+                if (`shape-transform-${transform}` in argv) {
+                    try {
+                        const transformConfigFile = argv[`shape-transform-${transform}`];
+                        const transformConfigJSON = fs.readFileSync(path.resolve(transformConfigFile), { encoding: 'utf8' });
+                        const transformConfig = transformConfigJSON.trim() ? JSON.parse(transformConfigJSON) : {};
+                        this.push(_.zipObject([transform], [transformConfig]));
+                    } catch {}
+                } else {
+                    this.push(transform);
+                }
+            }
+        }, config.shape.transform);
 }
 
 // Run through all sprite modes
-['css', 'view', 'defs', 'symbol', 'stack'].forEach(function (mode) {
-	if (!argv[mode] && !(mode in JSONConfig.mode)) {
-		delete this[mode];
-		return;
-	}
+['css', 'view', 'defs', 'symbol', 'stack'].forEach(function(mode) {
+    if (!argv[mode] && !(mode in JSONConfig.mode)) {
+        delete this[mode];
+        return;
+    }
 
-	// Remove excessive render types
-	['css', 'scss', 'less', 'styl'].forEach(function (render) {
-		var arg = mode + '-render-' + render;
-		if ((render in this) && !argv[arg] && (!(mode in JSONConfig.mode) || !('render' in JSONConfig.mode[mode]) || !(render in JSONConfig.mode[mode].render))) {
-			delete this[render];
-		}
-	}, this[mode].render);
+    // Remove excessive render types
+    ['css', 'scss', 'less', 'styl'].forEach(function(render) {
+        const arg = `${mode}-render-${render}`;
+        if (render in this && !argv[arg] && (!(mode in JSONConfig.mode) || !('render' in JSONConfig.mode[mode]) || !(render in JSONConfig.mode[mode].render))) {
+            delete this[render];
+        }
+    }, this[mode].render);
 
-	if (!this[mode].dimensions.length) {
-		this[mode].dimensions = true;
-	}
+    if (!this[mode].dimensions.length) {
+        this[mode].dimensions = true;
+    }
 }, config.mode);
 
 // Remove excessive example options
-for (var mode in config.mode) {
-	var example = mode + '-example';
-	if (!argv[example] && (!(mode in JSONConfig.mode) || !('example' in JSONConfig.mode[mode])) && ('example' in config.mode[mode])) {
-		delete config.mode[mode].example;
-	}
+for (const mode in config.mode) {
+    const example = `${mode}-example`;
+    if (!argv[example] && (!(mode in JSONConfig.mode) || !('example' in JSONConfig.mode[mode])) && 'example' in config.mode[mode]) {
+        delete config.mode[mode].example;
+    }
 }
 
 // Read & parse Mustache variable JSON file
 if ('variables' in config) {
-	var variables = ('' + config.variables).trim();
-	delete config.variables;
-	variables = variables.length ? path.resolve(variables) : null;
-	if (variables && fs.existsSync(variables)) {
-		try {
-			config.variables = JSON.parse(fs.readFileSync(variables));
-		} catch (e) {
-			console.error('[ERROR] Skipping --variables file due to errors ("%s")', e.message.trim());
-		}
-	}
+    let variables = String(config.variables).trim();
+    delete config.variables;
+    variables = variables.length ? path.resolve(variables) : null;
+    if (variables && fs.existsSync(variables)) {
+        try {
+            config.variables = JSON.parse(fs.readFileSync(variables));
+        } catch (error) {
+            console.error('[ERROR] Skipping --variables file due to errors ("%s")', error.message.trim());
+        }
+    }
 }
 
-var spriter = new SVGSpriter(config);
+const spriter = new SVGSpriter(config);
 
-argv._.reduce(function (f, g) {
-	return f.concat(glob.sync(g));
-}, []).forEach(function (file) {
-	var basename = file;
-	file = path.resolve(file);
-	var stat = fs.lstatSync(file);
-	if (stat.isSymbolicLink()) {
-		file = fs.readlinkSync(file);
-		basename = path.basename(file);
-	} else {
-		var basepos = basename.lastIndexOf('./');
-		basename = (basepos >= 0) ? basename.substr(basepos + 2) : path.basename(file);
-	}
-	spriter.add(file, basename, fs.readFileSync(file));
-});
+argv._.reduce((f, g) => [...f, ...glob.sync(g)], [])
+    .forEach(file => {
+        let basename = file;
+        file = path.resolve(file);
+        const stat = fs.lstatSync(file);
+        if (stat.isSymbolicLink()) {
+            file = fs.readlinkSync(file);
+            basename = path.basename(file);
+        } else {
+            const basepos = basename.lastIndexOf('./');
+            basename = basepos >= 0 ? basename.substr(basepos + 2) : path.basename(file);
+        }
 
-spriter.compile(function (error, result /*, data*/) {
-	if (error) {
-		console.error(error);
-	} else {
-		writeFiles(result);
-	}
+        spriter.add(file, basename, fs.readFileSync(file));
+    });
+
+spriter.compile((error, result) => {
+    if (error) {
+        console.error(error);
+    } else {
+        writeFiles(result);
+    }
 });
