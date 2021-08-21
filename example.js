dca9ae2db74084b46c5d8d1dcf28c7823f046d0d
XhmikosR <xhmikosr@gmail.com>
2021-08-21 16:16:18 +0300

    Update svgo to v2.4.0 (#501)
    * Update svgo to v2.4.0
    
    * example.js: add a custom SVGO config

--- example.js
+++ example.js
@@ -11,7 +11,34 @@ const files = glob.sync('**/weather*.svg', { cwd });
 
 const spriter = new SVGSpriter({
     dest,
-    log: 'debug'
+    log: 'debug',
+    shape: {
+        transform: [{
+            svgo: {
+                multipass: true,
+                plugins: [{
+                    name: 'preset-default',
+                    params: {
+                        overrides: {
+                            cleanupListOfValues: true,
+                            removeAttrs: {
+                                attrs: [
+                                    'data-name',
+                                    'fill',
+                                    'clip-rule'
+                                ]
+                            },
+                            removeUnknownsAndDefaults: {
+                                keepRoleAttr: true
+                            },
+                            removeViewBox: false,
+                            sortAttrs: true
+                        }
+                    }
+                }]
+            }
+        }]
+    }
 });
 
 /**
