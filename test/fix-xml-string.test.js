691399f017142cfcc14ebe1757e2f647eb35de82
Andrey Nelyubin <nelyubin_a_a@sunlight.net>
2022-02-13 17:38:24 +0300

    fix: fixing valid svg checking #454

---
+++ test/fix-xml-string.test.js
@@ -0,0 +1,35 @@
+const assert = require('assert');
+const fixXMLString = require('../lib/svg-sprite/fix-xml-string.js');
+
+describe('testing fixing svg string', () => {
+    it('should return valid svg file on svg with multiline attribute values', () => {
+        assert.equal(fixXMLString(`<svg viewBox="0 0 0 16
+                                     16"></svg>`), '<svg viewBox="0 0 0 16 16"/>');
+    });
+    it('should return valid svg file on svg with multiline attribute values', () => {
+        assert.equal(fixXMLString(`<svg fill="r
+                                                            e
+                                                            d"
+                                                            viewBox="0 0 0 16
+                                                                                                 16"></svg>`)
+        , '<svg fill="r e d" viewBox="0 0 0 16 16"/>');
+    });
+    it('should return valid svg file on svg with multiline attribute values', () => {
+        assert.equal(fixXMLString(`<svg fill="r
+                                                            e
+                                                            d"
+                                                            viewBox="0
+                                                            0
+                                                            16
+                                                            16"></svg>`),
+        '<svg fill="r e d" viewBox="0 0 16 16"/>');
+    });
+    it('should throw an error on invalid file', () => {
+        assert.throws(() => {
+            fixXMLString('<svg viewBox=></svg>');
+        }, Error);
+    });
+    it('should return same string on valid svg', () => {
+        assert.equal(fixXMLString('<svg viewBox="0 0 0 16 16"></svg>'), '<svg viewBox="0 0 0 16 16"/>');
+    });
+});
---
+++ test/svg-shape.test.js
@@ -0,0 +1,128 @@
+const assert = require('assert');
+const { Buffer } = require('buffer');
+const path = require('path');
+const fs = require('fs');
+const File = require('vinyl');
+const glob = require('glob');
+const getShape = require('../lib/svg-sprite/shape.js');
+const SVGSpriter = require('../lib/svg-sprite.js');
+
+describe('testing SVGShape initialization', () => {
+    it('should not throw an error on valid svg file with multiline attribute values', () => {
+        const spriter = new SVGSpriter({
+            shape: {
+                dest: 'svg'
+            }
+        });
+
+        assert.doesNotThrow(() => {
+            getShape(new File({
+                path: __dirname,
+                contents: Buffer.from(`<svg viewBox="0 0 0 16
+                                     16"></svg>`)
+            }), spriter);
+        }, Error);
+    });
+    it('should not throw an error on valid svg file with multiline attribute values', () => {
+        const spriter = new SVGSpriter({
+            shape: {
+                dest: 'svg'
+            }
+        });
+
+        assert.doesNotThrow(() => {
+            getShape(new File({
+                path: __dirname,
+                contents: Buffer.from(`<svg fill="r
+                                                            e
+                                                            d"
+                                                            viewBox="0 0 0 16
+                                                                                                 16"></svg>`)
+            }), spriter);
+        }, Error);
+    });
+    it('should not throw an error on valid svg file with mutliple multilined attritbutes values', () => {
+        const spriter = new SVGSpriter({
+            shape: {
+                dest: 'svg'
+            }
+        });
+
+        assert.doesNotThrow(() => {
+            getShape(new File({
+                path: __dirname,
+                contents: Buffer.from(`<svg fill="r
+                                                            e
+                                                            d"
+                                                            viewBox="0
+                                                            0
+                                                            0
+                                                            16
+                                                            16"></svg>`)
+            }), spriter);
+        }, Error);
+    });
+    it('should throw an error on invalid file', () => {
+        const spriter = new SVGSpriter({
+            shape: {
+                dest: 'svg'
+            }
+        });
+
+        assert.throws(() => {
+            getShape(new File({
+                path: __dirname,
+                contents: Buffer.from('<svg viewBox=></svg>')
+            }), spriter);
+        }, Error);
+    });
+    it('should throw an error on non-svg files', () => {
+        const spriter = new SVGSpriter({
+            shape: {
+                dest: 'svg'
+            }
+        });
+
+        assert.throws(() => {
+            getShape(new File({
+                path: __dirname,
+                contents: Buffer.from('<div class="test">123</div>')
+            }), spriter);
+        }, Error);
+    });
+
+    it('should not throw an error on valid svg file with normal values', () => {
+        const spriter = new SVGSpriter({
+            shape: {
+                dest: 'svg'
+            }
+        });
+
+        assert.doesNotThrow(() => {
+            getShape(new File({
+                path: __dirname,
+                contents: Buffer.from('<svg viewBox="0 0 0 16 16"></svg>')
+            }), spriter);
+        }, Error);
+    });
+    it('should not throw an error on actual valid svg files', () => {
+        const spriter = new SVGSpriter({
+            shape: {
+                dest: 'svg'
+            }
+        });
+
+        const cwdWeather = path.join(__dirname, 'fixture/svg/single');
+        const weather = glob.sync('**/weather*.svg', { cwd: cwdWeather });
+
+        weather.forEach(weatherFile => {
+            const svgFileBuffer = fs.readFileSync(path.join(cwdWeather, weatherFile));
+            assert.doesNotThrow(() => {
+                getShape(new File({
+                    path: __dirname,
+                    contents: svgFileBuffer
+                }), spriter);
+            }, Error);
+        });
+    });
+});
