9ad9c3328e5714062e6dfa2fbc007131f60cc8fc
Andrew <1666622+Kreeg@users.noreply.github.com>
2022-02-26 16:23:53 +0300

    changing async to sync render with resvg (#573)

--- test/calculate-svg-dimensions.test.js
+++ test/calculate-svg-dimensions.test.js
@@ -1,64 +1,64 @@
 'use strict';
 
 const assert = require('assert').strict;
-const { readFile } = require('fs').promises;
+const { readFileSync } = require('fs');
 const path = require('path');
 const calculateSvgDimensions = require('../lib/svg-sprite/calculate-svg-dimensions.js');
 
 describe('calculateSvgDimensions', () => {
-    it('should return the expected dimensions from 46x46 fixture', async() => {
+    it('should return the expected dimensions from 46x46 fixture', function() {
+        this.timeout(30_000);
         const svgFilePath = path.join(__dirname, 'fixture/svg/special/without-dims/46x46.svg');
-        const svg = await readFile(svgFilePath, 'utf-8');
-        const dimensions = await calculateSvgDimensions(svg);
+        const svg = readFileSync(svgFilePath, 'utf-8');
+        const dimensions = calculateSvgDimensions(svg);
         const expected = { width: 46, height: 46 };
 
         assert.deepEqual(dimensions, expected);
     });
 
-    it('should return the expected dimensions from 2048x2048 fixture', async() => {
+    it('should return the expected dimensions from 2048x2048 fixture', () => {
         const svgFilePath = path.join(__dirname, 'fixture/svg/special/without-dims/2048x2048.svg');
-        const svg = await readFile(svgFilePath, 'utf-8');
-        const dimensions = await calculateSvgDimensions(svg);
+        const svg = readFileSync(svgFilePath, 'utf-8');
+        const dimensions = calculateSvgDimensions(svg);
         const expected = { width: 2048, height: 2048 };
 
         assert.deepEqual(dimensions, expected);
     });
 
-    it('should return the expected dimensions from 32x32 fixture', async() => {
+    it('should return the expected dimensions from 32x32 fixture', () => {
         const svgFilePath = path.join(__dirname, 'fixture/svg/special/without-dims/32x32.svg');
-        const svg = await readFile(svgFilePath, 'utf-8');
-        const dimensions = await calculateSvgDimensions(svg);
+        const svg = readFileSync(svgFilePath, 'utf-8');
+        const dimensions = calculateSvgDimensions(svg);
         const expected = { width: 32, height: 32 };
 
         assert.deepEqual(dimensions, expected);
     });
 
-    it('should return the expected dimensions from 100x100 fixture', async() => {
+    it('should return the expected dimensions from 100x100 fixture', () => {
         const svgFilePath = path.join(__dirname, 'fixture/svg/special/without-dims/100x100.svg');
-        const svg = await readFile(svgFilePath, 'utf-8');
-        const dimensions = await calculateSvgDimensions(svg);
+        const svg = readFileSync(svgFilePath, 'utf-8');
+        const dimensions = calculateSvgDimensions(svg);
         const expected = { width: 100, height: 100 };
 
         assert.deepEqual(dimensions, expected);
     });
 
-    it('should return the expected dimensions from 231x69 fixture', async() => {
+    it('should return the expected dimensions from 231x69 fixture', () => {
         const svgFilePath = path.join(__dirname, 'fixture/svg/special/without-dims/231x69.svg');
-        const svg = await readFile(svgFilePath, 'utf-8');
-        const dimensions = await calculateSvgDimensions(svg);
+        const svg = readFileSync(svgFilePath, 'utf-8');
+        const dimensions = calculateSvgDimensions(svg);
         const expected = { width: 231, height: 69 };
 
         assert.deepEqual(dimensions, expected);
     });
 
-    it('should return same results no each run', async() => {
+    it('should return same results no each run', () => {
         const svgFilePath = path.join(__dirname, 'fixture/svg/special/without-dims/46x46.svg');
-        const svg = await readFile(svgFilePath, 'utf-8');
+        const svg = readFileSync(svgFilePath, 'utf-8');
 
-        const firstRunDimensions = await calculateSvgDimensions(svg);
-        const secondRunDimensions = await calculateSvgDimensions(svg);
-        const thirdRunDimensions = await calculateSvgDimensions(svg);
-
+        const firstRunDimensions = calculateSvgDimensions(svg);
+        const secondRunDimensions = calculateSvgDimensions(svg);
+        const thirdRunDimensions = calculateSvgDimensions(svg);
         assert.deepEqual(firstRunDimensions, secondRunDimensions);
         assert.deepEqual(thirdRunDimensions, secondRunDimensions);
     });
