c368ee1ed59689755ff3e6d665e47e085c9b010d
一丝 <yisibl@users.noreply.github.com>
2022-02-28 16:17:19 +0800

    feat: disable loadSystemFonts in resvg-js to improve first-time loading speed (#576)

--- test/helpers/resvg-preheat.js
+++
@@ -1,7 +0,0 @@
-// resvg preheat
-const resvg = require('@resvg/resvg-js');
-
-resvg.render('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"></svg>', {
-    fitTo: { mode: 'original' },
-    logLevel: 'error'
-});
