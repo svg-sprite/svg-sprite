f7d28f6a11c3aa2678c61e71a75e4022605daf0e
XhmikosR <xhmikosr@gmail.com>
2022-03-05 09:35:11 +0200

    Fix lint after 24d81aa

--- test/svg-sprite/minimal-configuration/modes/view.test.js
+++ test/svg-sprite/minimal-configuration/modes/view.test.js
@@ -21,12 +21,12 @@ const compareSvg2PngHelper = require('../../../helpers/compare-svg-2-png.js');
 /**
  * Rasterize an SVG file and compare it to an expected image
  *
- * @param {String} svg                SVG file path
- * @param {String} png                PNG file path
- * @param {String} expected           Expected PNG file path
- * @param {String} diff               Diff file path
+ * @param {string} svg                SVG file path
+ * @param {string} png                PNG file path
+ * @param {string} expected           Expected PNG file path
+ * @param {string} diff               Diff file path
  * @param {Function} done             Callback
- * @param {String} msg                Message
+ * @param {string} msg                Message
  */
 async function compareSvg2Png(svg, png, expected, diff, done, msg) {
     try {
