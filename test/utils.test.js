40905257d1105e32673064f16559c73bdbc7881d
Andrey Nelyubin <nelyubin_a_a@sunlight.net>
2022-03-07 16:15:42 +0300

    adding tests for isPlainObject, isObject. little refactoring and description tweaks

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,7 +1,9 @@
 'use strict';
 
+/* eslint-disable unicorn/new-for-builtins, no-new-wrappers, prefer-regex-literals */
+
 const assert = require('assert').strict;
-const { isFunction, isObject, isString } = require('../lib/svg-sprite/utils/index.js');
+const { isFunction, isObject, isString, isPlainObject } = require('../lib/svg-sprite/utils/index.js');
 
 describe('utils', () => {
     describe('isFunction', () => {
@@ -59,6 +61,26 @@ describe('utils', () => {
             assert.equal(isObject({}), true);
         });
 
+        it('should return true for a new String', () => {
+            assert.equal(isObject(new String('')), true);
+        });
+
+        it('should return true for a new Regexp', () => {
+            assert.equal(isObject(new RegExp('')), true);
+        });
+
+        it('should return true for a new Number', () => {
+            assert.equal(isObject(new Number(1)), true);
+        });
+
+        it('should return true for a new Boolean', () => {
+            assert.equal(isObject(new Boolean()), true);
+        });
+
+        it('should return false for a new Array', () => {
+            assert.equal(isObject(Array.from({ length: 1 })), false);
+        });
+
         it('should return false for an array', () => {
             assert.equal(isObject([1, 2, 3]), false);
         });
@@ -81,6 +103,14 @@ describe('utils', () => {
 
         it('should return false for a boolean value', () => {
             assert.equal(isObject(false), false);
+        });
+
+        it('should return false for a function', () => {
+            assert.equal(isObject(() => {}), false);
+        });
+
+        it('should return false for a Symbol', () => {
+            assert.equal(isObject(Symbol('test')), false);
         });
     });
 
@@ -107,6 +137,64 @@ describe('utils', () => {
 
         it('should return false for a boolean value', () => {
             assert.equal(isString(false), false);
+        });
+    });
+
+    describe('isPlainObject', () => {
+        it('should return true for an object', () => {
+            assert.equal(isPlainObject({ a: 1 }), true);
+        });
+
+        it('should return false for a new String', () => {
+            assert.equal(isPlainObject(new String('')), false);
+        });
+
+        it('should return false for a new Regexp', () => {
+            assert.equal(isPlainObject(new RegExp('')), false);
+        });
+
+        it('should return false for a new Number', () => {
+            assert.equal(isPlainObject(new Number(1)), false);
+        });
+
+        it('should return false for a new Boolean', () => {
+            assert.equal(isPlainObject(new Boolean()), false);
+        });
+
+        it('should return false for a new Array', () => {
+            assert.equal(isPlainObject(Array.from({ length: 1 })), false);
+        });
+
+        it('should return false for an array', () => {
+            assert.equal(isPlainObject([1, 2, 3]), false);
+        });
+
+        it('should return false for a Function constructor', () => {
+            assert.equal(isPlainObject(Function), false);
+        });
+
+        it('should return false for a null value', () => {
+            assert.equal(isPlainObject(null), false);
+        });
+
+        it('should return false for an undefined value', () => {
+            assert.equal(isPlainObject(undefined), false);
+        });
+
+        it('should return false for a string value', () => {
+            assert.equal(isPlainObject('test'), false);
+        });
+
+        it('should return false for a boolean value', () => {
+            assert.equal(isPlainObject(false), false);
+        });
+
+        it('should return false for a function', () => {
+            assert.equal(isPlainObject(() => {}), false);
+        });
+
+        it('should return false for a Symbol', () => {
+            assert.equal(isPlainObject(Symbol('test')), false);
         });
     });
 });
