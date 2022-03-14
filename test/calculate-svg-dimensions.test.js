2dfdfdfcad883b65332ee27b51ddb893be990579
XhmikosR <xhmikosr@gmail.com>
2022-03-14 17:57:29 +0200

    Update calculate-svg-dimensions.test.js

--- test/calculate-svg-dimensions.test.js
+++ test/calculate-svg-dimensions.test.js
@@ -10,8 +10,10 @@ describe('calculateSvgDimensions', () => {
 
         const svgFilePath = path.join(__dirname, 'fixture/svg/special/without-dims/46x46.svg');
         const svg = readFileSync(svgFilePath, 'utf-8');
+        const dimensions = calculateSvgDimensions(svg);
+        const expected = { width: 46, height: 46 };
 
-        expect(calculateSvgDimensions(svg)).toStrictEqual({ width: 46, height: 46 });
+        expect(dimensions).toStrictEqual(expected);
     });
 
     it('should return the expected dimensions from 2048x2048 fixture', () => {
@@ -19,8 +21,10 @@ describe('calculateSvgDimensions', () => {
 
         const svgFilePath = path.join(__dirname, 'fixture/svg/special/without-dims/2048x2048.svg');
         const svg = readFileSync(svgFilePath, 'utf-8');
+        const dimensions = calculateSvgDimensions(svg);
+        const expected = { width: 2048, height: 2048 };
 
-        expect(calculateSvgDimensions(svg)).toStrictEqual({ width: 2048, height: 2048 });
+        expect(dimensions).toStrictEqual(expected);
     });
 
     it('should return the expected dimensions from 32x32 fixture', () => {
@@ -28,8 +32,10 @@ describe('calculateSvgDimensions', () => {
 
         const svgFilePath = path.join(__dirname, 'fixture/svg/special/without-dims/32x32.svg');
         const svg = readFileSync(svgFilePath, 'utf-8');
+        const dimensions = calculateSvgDimensions(svg);
+        const expected = { width: 32, height: 32 };
 
-        expect(calculateSvgDimensions(svg)).toStrictEqual({ width: 32, height: 32 });
+        expect(dimensions).toStrictEqual(expected);
     });
 
     it('should return the expected dimensions from 100x100 fixture', () => {
@@ -37,8 +43,10 @@ describe('calculateSvgDimensions', () => {
 
         const svgFilePath = path.join(__dirname, 'fixture/svg/special/without-dims/100x100.svg');
         const svg = readFileSync(svgFilePath, 'utf-8');
+        const dimensions = calculateSvgDimensions(svg);
+        const expected = { width: 100, height: 100 };
 
-        expect(calculateSvgDimensions(svg)).toStrictEqual({ width: 100, height: 100 });
+        expect(dimensions).toStrictEqual(expected);
     });
 
     it('should return the expected dimensions from 231x69 fixture', () => {
@@ -46,8 +54,10 @@ describe('calculateSvgDimensions', () => {
 
         const svgFilePath = path.join(__dirname, 'fixture/svg/special/without-dims/231x69.svg');
         const svg = readFileSync(svgFilePath, 'utf-8');
+        const dimensions = calculateSvgDimensions(svg);
+        const expected = { width: 231, height: 69 };
 
-        expect(calculateSvgDimensions(svg)).toStrictEqual({ width: 231, height: 69 });
+        expect(dimensions).toStrictEqual(expected);
     });
 
     it('should return same results no each run', () => {
