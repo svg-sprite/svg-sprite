9765b9d48527328ebd2d73530a9343fd46a8685d
XhmikosR <xhmikosr@gmail.com>
2022-03-16 18:20:10 +0200

    Enable prettier

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    trailingComma: 'none',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    printWidth: 120,
+    plugins: ["prettier-plugin-jsdoc"],
+    jsdocVerticalAlignment: true,
+    jsdocSeparateTagGroups: true
+};
