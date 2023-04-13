73b2244e3b0d92ffe33997655d121e17bec369b4
XhmikosR <xhmikosr@gmail.com>
2023-04-13 10:15:05 +0300

    More

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-    trailingComma: 'none',
-    tabWidth: 2,
-    semi: true,
-    singleQuote: true,
-    bracketSpacing: true,
-    arrowParens: 'avoid',
-    printWidth: 80,
-    plugins: ["prettier-plugin-jsdoc"],
-    jsdocVerticalAlignment: true,
-    jsdocSeparateTagGroups: true
+  trailingComma: 'none',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  printWidth: 80,
+  plugins: ['prettier-plugin-jsdoc'],
+  jsdocVerticalAlignment: true,
+  jsdocSeparateTagGroups: true
 };
--- bin/svg-sprite.js
+++ bin/svg-sprite.js
@@ -3,17 +3,18 @@
 'use strict';
 
 /**
- * svg-sprite is a Node.js module for creating SVG sprites
+ * Svg-sprite is a Node.js module for creating SVG sprites
+ *
+ * @license MIT https://github.com/svg-sprite/svg-sprite/blob/main/LICENSE
+ *
+ * @author Joschi Kuphal [joschi@kuphal.net](mailto:joschi@kuphal.net)
+ *   (https://github.com/jkphl)
+ * @copyright © 2018 Joschi Kuphal
  *
  * @see https://github.com/svg-sprite/svg-sprite
- * @author Joschi Kuphal <joschi@kuphal.net> (https://github.com/jkphl)
- * @copyright © 2018 Joschi Kuphal
- * @license MIT https://github.com/svg-sprite/svg-sprite/blob/main/LICENSE
  */
 
-/**
- * Module dependencies.
- */
+/** Module dependencies. */
 const fs = require('node:fs');
 const path = require('node:path');
 const merge = require('lodash.merge');
@@ -25,13 +26,21 @@ const SVGSpriter = require('../lib/svg-sprite.js');
 const { isObject, zipObject } = require('../lib/svg-sprite/utils/index.js');
 
 yargs
-  .usage('Create one or multiple sprites of the given SVG files, optionally along with some stylesheet resources.\nUsage: $0 [options] files')
+  .usage(
+    'Create one or multiple sprites of the given SVG files, optionally along with some stylesheet resources.\nUsage: $0 [options] files'
+  )
   .version()
   .help('help', 'Display this help information')
   .wrap(null)
-  .example('$0 --css --css-render-css --css-example --dest=out assets/*.svg', 'Create a CSS sprite of the given SVG files including example document to the subdirectory "out"')
+  .example(
+    '$0 --css --css-render-css --css-example --dest=out assets/*.svg',
+    'Create a CSS sprite of the given SVG files including example document to the subdirectory "out"'
+  )
   .example('$0 -cD out --ccss --cx assets/*.svg', 'Same as above')
-  .example('$0 -cD out --cscss -p 10 assets/*.svg', 'Render Sass instead of CSS and add 10px padding around all shapes (no example document this time)')
+  .example(
+    '$0 -cD out --cscss -p 10 assets/*.svg',
+    'Render Sass instead of CSS and add 10px padding around all shapes (no example document this time)'
+  )
   .showHelpOnFail(true)
   .demandCommand(1);
 
@@ -42,7 +51,7 @@ const optionsMap = {};
 /**
  * Add a command line option
  *
- * @param {string} name Option name
+ * @param {string} name   Option name
  * @param {object} option Option configuration
  */
 function addOption(name, option) {
@@ -59,7 +68,9 @@ function addOption(name, option) {
 
     if ('default' in option) {
       const template = name.endsWith('-template');
-      const def = template ? path.resolve(path.dirname(__dirname), option.default) : option.default;
+      const def = template
+        ? path.resolve(path.dirname(__dirname), option.default)
+        : option.default;
       yargs = yargs.default(alias, def);
 
       if (option.default === true || option.default === false) {
@@ -74,7 +85,13 @@ function addOption(name, option) {
     }
   }
 
-  const { description, alias: optAlias, default: optDefault, map, ...children } = option;
+  const {
+    description,
+    alias: optAlias,
+    default: optDefault,
+    map,
+    ...children
+  } = option;
   for (const [key, value] of Object.entries(children)) {
     addOption(`${name}-${key}`, value);
   }
@@ -83,9 +100,9 @@ function addOption(name, option) {
 /**
  * Add a value to the global configuration
  *
- * @param {object} store Configuration
- * @param {Array<string>} path Path
- * @param {any} value Value
+ * @param {object}   store Configuration
+ * @param {string[]} path  Path
+ * @param {any}      value Value
  */
 function addConfigMap(store, path, value) {
   const key = path.shift();
@@ -103,8 +120,9 @@ function addConfigMap(store, path, value) {
 /**
  * Recursively write files to disc
  *
- * @param {object} files Files
- * @returns {number}   Number of written files
+ * @param   {object} files Files
+ *
+ * @returns {number}       Number of written files
  */
 function writeFiles(files) {
   let written = 0;
@@ -128,7 +146,9 @@ function writeFiles(files) {
 
 // Get document, or throw exception on error
 try {
-  const options = yaml.load(fs.readFileSync(path.resolve(__dirname, 'config.yaml'), 'utf8'));
+  const options = yaml.load(
+    fs.readFileSync(path.resolve(__dirname, 'config.yaml'), 'utf8')
+  );
   for (const [key, value] of Object.entries(options)) {
     addOption(key, value);
   }
@@ -179,22 +199,35 @@ if (argv.config) {
 
     merge(config, externalConfig);
   } catch (error) {
-    console.error('[ERROR] Skipping --config file due to errors ("%s")', error.message.trim());
+    console.error(
+      '[ERROR] Skipping --config file due to errors ("%s")',
+      error.message.trim()
+    );
   }
 }
 
 // Refine particular config options
 config.shape.spacing.padding = String(config.shape.spacing.padding).trim();
-config.shape.spacing.padding = config.shape.spacing.padding.length ?
-  config.shape.spacing.padding.split(',').map(dim => Number.parseFloat(dim || 0)) :
-  [];
+config.shape.spacing.padding = config.shape.spacing.padding.length
+  ? config.shape.spacing.padding
+      .split(',')
+      .map(dim => Number.parseFloat(dim || 0))
+  : [];
 
-if (config.svg.rootAttributes && typeof config.svg.rootAttributes === 'string') {
+if (
+  config.svg.rootAttributes &&
+  typeof config.svg.rootAttributes === 'string'
+) {
   try {
-    const JSONAttributesContent = fs.readFileSync(path.resolve(config.svg.rootAttributes));
+    const JSONAttributesContent = fs.readFileSync(
+      path.resolve(config.svg.rootAttributes)
+    );
     config.svg.rootAttributes = JSON.parse(JSONAttributesContent);
   } catch (error) {
-    console.error('[ERROR] Skipping --svg-rootattrs file due to errors ("%s")', error.message.trim());
+    console.error(
+      '[ERROR] Skipping --svg-rootattrs file due to errors ("%s")',
+      error.message.trim()
+    );
     config.svg.rootAttributes = {};
   }
 }
@@ -203,15 +236,24 @@ if (config.svg.rootAttributes && typeof config.svg.rootAttributes === 'string') 
 if (typeof config.shape.transform === 'string') {
   const transform = String(config.shape.transform).trim();
   config.shape.transform = [];
-  (transform.length ? transform.split(',').map(trans => String(trans).trim()) : [])
+  (transform.length
+    ? transform.split(',').map(trans => String(trans).trim())
+    : []
+  )
     // TODO
-    .forEach(function(transform) { // eslint-disable-line unicorn/no-array-for-each
+    // eslint-disable-next-line unicorn/no-array-for-each
+    .forEach(function (transform) {
       if (transform.length) {
         if (`shape-transform-${transform}` in argv) {
           try {
             const transformConfigFile = argv[`shape-transform-${transform}`];
-            const transformConfigJSON = fs.readFileSync(path.resolve(transformConfigFile), 'utf8');
-            const transformConfig = transformConfigJSON.trim() ? JSON.parse(transformConfigJSON) : {};
+            const transformConfigJSON = fs.readFileSync(
+              path.resolve(transformConfigFile),
+              'utf8'
+            );
+            const transformConfig = transformConfigJSON.trim()
+              ? JSON.parse(transformConfigJSON)
+              : {};
             this.push(zipObject([transform], [transformConfig]));
           } catch {}
         } else {
@@ -224,7 +266,7 @@ if (typeof config.shape.transform === 'string') {
 // Run through all sprite modes
 // TODO
 // eslint-disable-next-line unicorn/no-array-for-each
-['css', 'view', 'defs', 'symbol', 'stack'].forEach(function(mode) {
+['css', 'view', 'defs', 'symbol', 'stack'].forEach(function (mode) {
   if (!argv[mode] && !(mode in JSONConfig.mode)) {
     delete this[mode];
     return;
@@ -233,9 +275,15 @@ if (typeof config.shape.transform === 'string') {
   // Remove excessive render types
   // TODO
   // eslint-disable-next-line unicorn/no-array-for-each
-  ['css', 'scss', 'less', 'styl'].forEach(function(render) {
+  ['css', 'scss', 'less', 'styl'].forEach(function (render) {
     const arg = `${mode}-render-${render}`;
-    if (render in this && !argv[arg] && (!(mode in JSONConfig.mode) || !('render' in JSONConfig.mode[mode]) || !(render in JSONConfig.mode[mode].render))) {
+    if (
+      render in this &&
+      !argv[arg] &&
+      (!(mode in JSONConfig.mode) ||
+        !('render' in JSONConfig.mode[mode]) ||
+        !(render in JSONConfig.mode[mode].render))
+    ) {
       delete this[render];
     }
   }, this[mode].render);
@@ -248,7 +296,11 @@ if (typeof config.shape.transform === 'string') {
 // Remove excessive example options
 for (const mode of Object.keys(config.mode)) {
   const example = `${mode}-example`;
-  if (!argv[example] && (!(mode in JSONConfig.mode) || !('example' in JSONConfig.mode[mode])) && 'example' in config.mode[mode]) {
+  if (
+    !argv[example] &&
+    (!(mode in JSONConfig.mode) || !('example' in JSONConfig.mode[mode])) &&
+    'example' in config.mode[mode]
+  ) {
     delete config.mode[mode].example;
   }
 }
@@ -262,7 +314,10 @@ if ('variables' in config) {
     try {
       config.variables = JSON.parse(fs.readFileSync(variables));
     } catch (error) {
-      console.error('[ERROR] Skipping --variables file due to errors ("%s")', error.message.trim());
+      console.error(
+        '[ERROR] Skipping --variables file due to errors ("%s")',
+        error.message.trim()
+      );
     }
   }
 }
@@ -280,7 +335,8 @@ for (let file of files) {
     basename = path.basename(file);
   } else {
     const basepos = basename.lastIndexOf('./');
-    basename = basepos >= 0 ? basename.substr(basepos + 2) : path.basename(file);
+    basename =
+      basepos >= 0 ? basename.substr(basepos + 2) : path.basename(file);
   }
 
   spriter.add(file, basename, fs.readFileSync(file));
