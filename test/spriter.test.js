svg-sprite/svg-sprite

commit 23958e8c9addb99036378e6a90e156f58797aacf

refactor: drop `lodash.trim` dependency (#749)
* refactor: drop lodash.trim dependency

Use JavaScript's built-in String.prototype.trim method instead of
lodash.trim, reducing dependency bloat.

In one case, we were calling lodash.trim with a Buffer instance.
lodash.trim calls toString on its argument, so we didn't notice this
issue before. Continue calling toString before calling
String.prototype.trim to preserve behavior.

* Update comment

* Update package-lock.json

* Call toString if isBuffer

* Add default function parameters to prevent TypeErrors

* Update svg-sprite.js

Co-authored-by: Matthew "strager" Glazar <strager.nds@gmail.com>
Co-authored-by: Andrew <1666622+Kreeg@users.noreply.github.com>

lib/svg-sprite.js
@@ -14,7 +14,6 @@ const path = require('path');
 const events = require('events');
 const os = require('os');
 const { format } = require('util');
-const trim = require('lodash.trim');
 const trimStart = require('lodash.trimstart');
 const merge = require('lodash.merge');
 const File = require('vinyl');
@@ -75,24 +74,29 @@ SVGSpriter.prototype._namespacePow = [];
 /**
  * Add an SVG shape to the sprites
  *
- * @param {File | string} file             Vinyl file object or absolute file path
- * @param {string} name                    Name part of the file path
- * @param {Buffer | string} svg            SVG content
- * @returns {SVGSpriter}                   Self reference
- * @throws {Error}                         In case an invalid file should be added
+ * @param {File | string} [file]             Vinyl file object or absolute file path
+ * @param {string} [name]                    Name part of the file path
+ * @param {Buffer | string} [svg]            SVG content
+ * @returns {SVGSpriter}                     Self reference
+ * @throws {Error}                           In case an invalid file is added
  */
-SVGSpriter.prototype.add = function(file, name, svg) {
+SVGSpriter.prototype.add = function(file = '', name = '', svg = '') {
     // If no vinyl file object has been given
     if (!this._isVinylFile(file)) {
-        file = trim(file);
+        file = file.trim();
         let error = null;
 
         // If the name part of the file path is absolute
         if (name && path.isAbsolute(name)) {
             error = format('SVGSpriter.add: "%s" is not a valid relative file name', name);
         } else {
-            name = trimStart(trim(name), `${path.sep}.`) || path.basename(file);
-            svg = trim(svg);
+            name = trimStart(name.trim(), `${path.sep}.`) || path.basename(file);
+            // TODO: Avoid Buffer -> String -> Buffer conversion of svg.
+            if (Buffer.isBuffer(svg)) {
+                svg = svg.toString();
+            }
+
+            svg = svg.trim();
 
             // Argument validation
             if (arguments.length < 3) {

commit ccb0310d47f7c7c8f4026a8f7999dbf2810b78a7

fix: fix crash with spriter.add(..., null, ...)
README.md has an example of calling spriter.add with name=null.
Unfortunately, Git commit 23958e8c broke that case; calling spriter.add
with name=null leads to an exception.

Make the code work like it did prior to commit 23958e8c: allow null,
undefined, and empty strings.

Refs: 23958e8c9addb99036378e6a90e156f58797aacf

lib/svg-sprite.js
@@ -92,7 +92,11 @@ SVGSpriter.prototype.add = function(file = '', name = '', svg = '') {
     if (name && path.isAbsolute(name)) {
       errorMessage = format('SVGSpriter.add: "%s" is not a valid relative file name', name);
     } else {
-      name = trimStart(name.trim(), `${path.sep}.`) || path.basename(file);
+      if (name !== null) {
+        name = trimStart(name.trim(), `${path.sep}.`);
+      }
+
+      name = name || path.basename(file);
       // TODO: Avoid Buffer -> String -> Buffer conversion of svg.
       if (Buffer.isBuffer(svg)) {
         svg = svg.toString();

test/spriter.test.js
@@ -98,6 +98,23 @@ describe('testing SVGSpriter', () => {
         }));
       });
 
+      it.each([
+        [null],
+        [undefined],
+        ['']
+      ])('should create vinyl file with file name if name is %p', name => {
+        spriter._queue = {
+          add: jest.fn()
+        };
+        spriter.add(TEST_SVG, name, TEST_EMPTY_SVG);
+
+        expect(spriter._queue.add).toHaveBeenCalledWith(new File({
+          base: path.dirname(path.resolve(TEST_SVG)),
+          path: path.resolve(TEST_SVG),
+          contents: Buffer.from(TEST_EMPTY_SVG)
+        }));
+      });
+
       it('should create vinyl file from passed absolute file and add it to _queue', () => {
         spriter._queue = {
           add: jest.fn()